444a3b7e74bc940b2f64c4f049de8819f13460be
Zhiyu Wang <zhiyu_wang_work@outlook.com>
2026-01-08 12:17:33 +0800

    adapt for my server

--- config/eval_gpt2.py
+++ config/eval_gpt2.py
@@ -1,8 +1,8 @@
 # evaluate the base gpt2
 # n_layer=12, n_head=12, n_embd=768
 # 124M parameters
-batch_size = 8
+batch_size = 256
 eval_iters = 500 # use more iterations to get good estimate
 eval_only = True
-wandb_log = False
+wandb_log = True
 init_from = 'gpt2'
